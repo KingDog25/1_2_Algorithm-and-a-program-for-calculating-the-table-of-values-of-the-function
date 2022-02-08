b05386362310c9428d5a2267cb81ac8ee638ec9f
Alexandr Roslyakov <99288384+KingDog25@users.noreply.github.com>
2022-02-09 02:24:56 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace _23_variant_CSharp_1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableName = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.RowHeadersWidth = 47;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 328);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tableName
+            // 
+            this.tableName.AutoSize = true;
+            this.tableName.Font = new System.Drawing.Font("Bell MT", 15.70909F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tableName.Location = new System.Drawing.Point(302, 331);
+            this.tableName.Name = "tableName";
+            this.tableName.Size = new System.Drawing.Size(260, 27);
+            this.tableName.TabIndex = 1;
+            this.tableName.Text = "Таблица значений функции";
+            this.tableName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.tableName.Click += new System.EventHandler(this.tableName_Text);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.163636F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(351, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Рассчитать значения";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.ImageLocation = "C:\\Users\\Александр\\source\\repos\\23_variant_CSharp_1_2\\images\\Formula.bmp";
+            this.pictureBox1.Location = new System.Drawing.Point(568, 349);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 77);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label tableName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿/*Составить алгоритм и программу вычисления таблицы значений функции f(x) из задачи 1.1 для N значений
+аргумента X, равномерно распределенных на отрезке [А,B].Для проверки программы задать N=10; A=0,55; B=1.*/
+
+/*Compose an algorithm and a program for calculating the table of values of the function f(x) from problem 1.1 for N values
+argument X, evenly distributed on the segment [A, B]. To test the program, set N=10; A=0.55; B=1.*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23_variant_CSharp_1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+ 
+        public static double functValTable(double x)
+        {
+            double y;
+            y = Math.Log(Math.Sqrt(Math.Pow(Math.E, 0.1 * x) + x)) / (x + Math.Pow(10.7, 1 / 3) + Math.Atan(x) + 2 / 5);
+            // ln(sqrt(e ^ 0.1x + x)) / (x + pow(10.7, 1 / 3) + arctg(x) + 2 / 5)
+            return y;
+        }
+
+        private void tableName_Text(object sender, EventArgs e)
+        {
+            tableName.Text = "Таблица значений функции";
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            double[,] tableXY = new double[10, 2];
+            dataGridView1.RowCount = 10;
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].HeaderText = "X";
+            dataGridView1.Columns[1].HeaderText = "Y";
+            double a=0.55,x=a, b=1, step = 0.03;
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                for (int j = 0; j < 1; j++)                                         //заполняем значения X
+                {
+                   tableXY[i, j] = x;
+                    dataGridView1.Rows[i].Cells[j].Value = tableXY[i, j].ToString("0.####");
+                    x += step;                                                       //прибавляем шаг
+                }
+            }
+            x=a;                                                                    //сбросить шаг
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 1; j < 2; j++)
+                {
+                    tableXY[i, j] = functValTable(x);
+                    dataGridView1.Rows[i].Cells[j].Value = tableXY[i, j].ToString("0.####");
+                    x += step;
+                }
+            }
+        
+        }
+    }
+}
